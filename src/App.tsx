4c1e8f1302c20845a2298c595d7c70c51fa17d62
Ihar Anikeyenka <ihar.anikeyenka@gmail.com>
2024-06-23 23:41:05 +0200

    feat: the first step

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-import '@mantine/core/styles.css'
+import { createTheme, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const theme = createTheme({
+    fontFamily: "Open Sans, sans-serif",
+    primaryColor: "cyan",
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <MantineProvider theme={theme}>Test</MantineProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+//
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCyznN0G8P6KGhNIY1o104Vn2ZH06yCing",
+//     authDomain: "club-app-3822b.firebaseapp.com",
+//     projectId: "club-app-3822b",
+//     storageBucket: "club-app-3822b.appspot.com",
+//     messagingSenderId: "742423654420",
+//     appId: "1:742423654420:web:b7cd2ed0a74e9f9330b701",
+//     measurementId: "G-GBBJW7TY7W"
+// };
+//
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
